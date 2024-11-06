8b9e520cc9bd522f49dbb2aaca9a979c97c1ad58
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2024-11-05 23:11:31 -0300

    Obtiene el link de descarga

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -1,10 +1,14 @@
 const solicitud = require('../models/solicitudInspeccion');
 var viviendas=require('../models/viviendas');
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 // Generar un pdf
 const generarPdf = async (req, res) => {
     var id_solicitud=req.params.id_solicitud
-    const API_URL = "https://api.pdfmonkey.io/api/v1/documents"
+    const API_URL = "https://api.pdfmonkey.io/api/v1"
+    const MAX_INTENTOS = 5;
+    let intentos = 0;
+
     try {
         const unaSolicitud = await solicitud.findOne({
             where: {
@@ -28,14 +32,37 @@ const generarPdf = async (req, res) => {
         bodyApi.document.meta._filename = "Solicitud_"+unaSolicitud.Vivienda.numero_expediente+".pdf";
         bodyApi.document.status= "pending" //Estoy creando un documento con "pending"
         
-        try{
-            const respuesta = await fetch(API_URL, {
+        try{ // Primera llamada para crear el documento y obtener el ID
+            const respuesta = await fetch(API_URL+"/documents", {
                 method: 'POST',
                 headers:{'Content-Type': 'application/json','Authorization': `Bearer ${process.env.PDF_TOKEN}`},
                 body:JSON.stringify(bodyApi)
             });
-            const data = await respuesta.json();
-            res.status(respuesta.status).json(data.document.preview_url); //Devuelve la preview, no el link de descarga
+            const dataDocumento = await respuesta.json();
+            idDocumento = dataDocumento.document.id;
+
+            if (!idDocumento){ //
+                return res.status(500).json({ message: 'No se pudo obtener el ID del documento.' });
+            }
+
+            while (intentos <= MAX_INTENTOS){
+                intentos++;
+                console.log(`Intento ${intentos}: verificando el estado del documento...`);
+
+                await delay(1000); //Espero 1 segundo
+                const respuestaGet= await fetch(API_URL+"/document_cards/"+idDocumento, { //Llamada GET
+                    method: 'GET',
+                    headers:{'Content-Type': 'application/json','Authorization': `Bearer ${process.env.PDF_TOKEN}`}
+                });
+
+                console.log("res: ", respuestaGet);
+
+                dataGet = await respuestaGet.json();
+
+                if(dataGet.document_card.status ==="success"){
+                    return res.status(200).json (dataGet.document_card.download_url);//Devuelve el link de descarga
+                }
+            }
         } catch (error){
             console.error('Error al intentar llamar API PDF', error);
             res.status(500).json({message: 'Error al intentar llamar API PDF'})
